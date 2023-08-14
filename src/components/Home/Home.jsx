9fd3881addde3e3d4a3c04d287be16a7f4efc432
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2023-08-13 20:00:11 -0500

    hero page styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,12 +3,13 @@ import heroImg from "../../../src/assets/selfie.jpg"
 
 const Home = () => {
   return (
-    <section id="home" className='w-full pt-40 h-screen bg-backgroundColor'>
-        <div className='max-w-[1000px] mx-auto px-8 md:flex md:flex-row sm:flex-col justify-between h-full'>
-            <img className="rounded-full md:basis-1/3" src={heroImg} />
-            <div className='w-full md:basis-1/2 align-'>
+    <section id="home" className='w-full pt-40 h-screen flex justify-center bg-backgroundColor'>
+        <div className='max-w-[1000px] md:flex md:flex-row sm:flex-col justify-between h-full'>
+            <img className="rounded-full p-5" src={heroImg} />
+            <div className='w-full px-8 items-center'>
                 <h1>Hello world</h1>
-                <p>My name is Peter Geipel.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, eos vitae, asperiores explicabo modi ab ut autem fugit ducimus expedita sunt, voluptatem delectus mollitia dolore accusamus. Laborum voluptates eius sunt?</p>
+                
+                <p className='py-20'>My name is Peter Geipel.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, eos vitae, asperiores explicabo modi ab ut autem fugit ducimus expedita sunt, voluptatem delectus mollitia dolore accusamus. Laborum voluptates eius sunt?</p>
             </div>
             
         </div>
